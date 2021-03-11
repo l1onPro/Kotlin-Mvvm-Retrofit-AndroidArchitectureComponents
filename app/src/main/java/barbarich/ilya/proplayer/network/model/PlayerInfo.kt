dd7939fb6bfc2d8b235d9bc8b693ee28c86b8d95
Ilya Barbarich <ilya_barbarich@mail.ru>
2021-03-11 15:09:29 +0300

    add data

---
+++ app/src/main/java/barbarich/ilya/proplayer/network/model/PlayerInfo.kt
@@ -0,0 +1,23 @@
+package barbarich.ilya.proplayer.network.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class PlayerInfo (
+    var id: String,
+    var nickName: String,
+    var fullName: String,
+    var age: Int,
+    val imageSmallAvatar: String,
+    val imageBig: String,
+    var rating_1_0: Float,
+    var rating_2_0: Float,
+    var dpr: Float,
+    var kast: Float,
+    var impact: Float,
+    var adr: Float,
+    var kdp: Float,
+    var rating: Float,
+    var description: String
+) :  Parcelable
