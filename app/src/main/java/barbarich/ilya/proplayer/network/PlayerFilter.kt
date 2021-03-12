de823d614613f72e1da1edbda8d3975c21cae713
Ilya Barbarich <ilya_barbarich@mail.ru>
2021-03-12 12:19:51 +0300

    add new filter

--- app/src/main/java/barbarich/ilya/proplayer/network/PlayerFilter.kt
+++ app/src/main/java/barbarich/ilya/proplayer/network/PlayerFilter.kt
@@ -1,6 +1,7 @@
 package barbarich.ilya.proplayer.network
 
 enum class PlayerFilter(val value: String) {
+    SORT_BY_RATING_1_0 (value = "rating_1_0"),
     SORT_BY_RATING(value = "rating"),
-    SORT_BY_RATING_1_0 (value = "rating_1_0")
+    SORT_BY_NAME(value = "name")
 }
