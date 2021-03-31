4bd80115d451c03a9d01e3891d0795de121af655
Ilya Barbarich <ilya_barbarich@mail.ru>
2021-04-01 02:15:25 +0300

    fix

--- app/src/main/java/barbarich/ilya/proplayer/redux/state/State.kt
+++ app/src/main/java/barbarich/ilya/proplayer/redux/state/State.kt
@@ -1,11 +1,12 @@
 package barbarich.ilya.proplayer.redux.state
 
 import barbarich.ilya.proplayer.network.model.PlayerInfo
+import org.rekotlin.StateType
 
 data class AppState(
     val players: PlayersState = PlayersState()
-)
+) : StateType
 
 data class PlayersState (
     val players: List<PlayerInfo> = listOf()
-)
+) : StateType
