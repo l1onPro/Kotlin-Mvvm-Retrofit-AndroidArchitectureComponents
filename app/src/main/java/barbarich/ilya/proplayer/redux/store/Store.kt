b441ab60bb1835a6300fc8936603a93806e16e51
Ilya Barbarich <ilya_barbarich@mail.ru>
2021-04-01 02:15:17 +0300

    Add Request action

--- app/src/main/java/barbarich/ilya/proplayer/redux/store/Store.kt
+++ app/src/main/java/barbarich/ilya/proplayer/redux/store/Store.kt
@@ -1,10 +1,12 @@
 package barbarich.ilya.proplayer.redux.store
 
+import barbarich.ilya.proplayer.redux.middleware.appMiddleware
 import barbarich.ilya.proplayer.redux.reducer.appReducer
 import barbarich.ilya.proplayer.redux.state.AppState
 import org.rekotlin.Store
 
 val store = Store (
     reducer = ::appReducer,
-    state = AppState()
+    state = AppState(),
+    middleware = listOf(appMiddleware)
 )
